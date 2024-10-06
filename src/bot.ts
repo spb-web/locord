d747b5510ae99c5724fee76f6e0bdb15c8cdf4f2
Vyacheslav Ruzanov <spb.web@bk.ru>
2024-10-07 00:00:47 +0300

    add AUDIO_STREAM_URL

--- src/bot.ts
+++ src/bot.ts
@@ -4,9 +4,6 @@ import { DiscordVoiceChat } from './DiscordVoiceChat'
 import { DiscordAudioStream } from './DiscordAudioStream'
 import { runServer } from './server'
 
-const URL =
-  'https://fluxfm.streamabc.net/flx-chillhop-mp3-128-8581707?sABC=6701o016%230%234o035o52p5n23r25snqon3598r4op036%23fgernzf.syhksz.qr&aw_0_1st.playerid=streams.fluxfm.de&amsparams=playerid:streams.fluxfm.de;skey:1728163862'
-
 const run = async () => {
   const client = new DiscordClient(env.DISCORD_TOKEN)
   const voiceChat = new DiscordVoiceChat(client, env.CHANNEL_ID)
@@ -16,7 +13,7 @@ const run = async () => {
   await voiceChat.joinChannel()
   await voiceChat.play(audioStream)
 
-  audioStream.setSource(URL)
+  audioStream.setSource(env.AUDIO_STREAM_URL)
 
   runServer()
 }
--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ require('dotenv').config()
 const envCondition = z.object({
   DISCORD_TOKEN: z.string(),
   CHANNEL_ID: z.string(),
+  AUDIO_STREAM_URL: z.string(),
 })
 
 export const env = envCondition.parse(process.env)
